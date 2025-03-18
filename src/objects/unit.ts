68691a888a92c067bad9ab3c9887d6ce9e6e86df
Philipp Sorio <72080285+athphil@users.noreply.github.com>
2025-03-18 13:32:30 +0100

    feat(unit): adds select logic and changes speed from grid to pixel

--- src/objects/unit.ts
+++ src/objects/unit.ts
@@ -8,14 +8,26 @@ type IUnit = {
 export default class Unit extends Phaser.GameObjects.Sprite {
     public speed: number;
     public playerId: number;
+    public selected: boolean = false;
+    public baseTexture: string;
 
     constructor(scene: Phaser.Scene, x: number, y: number, texture: string, unitInfo: IUnit) {
         super(scene, x, y, texture);
         this.speed = unitInfo.speed;
         this.playerId = unitInfo.playerId;
+        this.baseTexture = texture;
         scene.add.existing(this);
     }
 
+    select() {
+        this.selected = true;
+        this.setTexture('unit_selected');
+    }
+
+    deselect() {
+        this.selected = false;
+        this.setTexture(this.baseTexture);
+    }
     // Weitere Methoden für die Einheit
     // angreifen(ziel: Unit) {
     //     // Logik für den Angriff
--- src/scenes/BattlemapScene.ts
+++ src/scenes/BattlemapScene.ts
@@ -6,7 +6,7 @@ const DBG_GAP_BETWEEN_UNITS = GRID_SIZE
 
 
 class BattlemapScene extends Phaser.Scene {
-    public player1Units: Phaser.GameObjects.Sprite[] = []
+    public player1Units: Unit[] = []
 
     public draggedStartX: number = -1
     public draggedStartY: number = -1
@@ -19,6 +19,7 @@ class BattlemapScene extends Phaser.Scene {
         this.load.image('bg', 'https://labs.phaser.io/assets/skies/deepblue.png');
         this.load.image('unit_green', 'public/sprites/Unit_Green.png');
         this.load.image('unit_red', 'public/sprites/Unit_Red.png');
+        this.load.image('unit_selected', 'public/sprites/Unit_Green.png');
     }
 
     create() {
@@ -27,39 +28,50 @@ class BattlemapScene extends Phaser.Scene {
         for (let i = 0; i < DBG_UNIT_PER_SIDE; i++) {
             const x = DBG_GAP_BETWEEN_UNITS + (i * GRID_SIZE) + (i * DBG_GAP_BETWEEN_UNITS)
             const y = GRID_SIZE
-            const unit = new Unit(this, x, y, 'unit_green', { speed: 100, playerId: 1 }).setOrigin(0, 0).setDisplaySize(GRID_SIZE, GRID_SIZE);
+            const unit = new Unit(this, x, y, 'unit_red', { speed: 100, playerId: 1 }).setOrigin(0, 0).setDisplaySize(GRID_SIZE, GRID_SIZE);
             unit.setInteractive({ draggable: true });
+            unit.on('pointerdown', () => {
+                //Refactor Loop
+                this.player1Units.forEach(unit => {
+                    unit.deselect();
+                })
+                unit.select()
+            });
             this.player1Units.push(unit)
         }
 
-        this.input.on('dragstart', (_pointer: Phaser.Input.Pointer, sprite: Unit) => {
-            this.draggedStartX = sprite.x;
-            this.draggedStartY = sprite.y;
+        // DRAG LOGIC START - To be modularized
+        {
+            this.input.on('dragstart', (_pointer: Phaser.Input.Pointer, sprite: Unit) => {
+                this.draggedStartX = sprite.x;
+                this.draggedStartY = sprite.y;
 
-            console.log("Start dragging - X: " + this.draggedStartX + " Y: " + this.draggedStartY);
-        });
+                console.log("Start dragging - X: " + this.draggedStartX + " Y: " + this.draggedStartY);
+            });
 
-        this.input.on('drag', (_pointer: Phaser.Input.Pointer, sprite: Phaser.GameObjects.Sprite, dragX: number, dragY: number) => {
-            dragX = Phaser.Math.Snap.To(dragX, GRID_SIZE);
-            dragY = Phaser.Math.Snap.To(dragY, GRID_SIZE);
+            this.input.on('drag', (_pointer: Phaser.Input.Pointer, sprite: Phaser.GameObjects.Sprite, dragX: number, dragY: number) => {
+                dragX = Phaser.Math.Snap.To(dragX, GRID_SIZE);
+                dragY = Phaser.Math.Snap.To(dragY, GRID_SIZE);
 
-            sprite.setPosition(dragX, dragY);
-        });
+                sprite.setPosition(dragX, dragY);
+            });
 
 
-        this.input.on('dragend', (_pointer: Phaser.Input.Pointer, sprite: Unit) => {
-            let distance = Phaser.Math.Distance.Between(this.draggedStartX, this.draggedStartY, sprite.x, sprite.y);
+            this.input.on('dragend', (_pointer: Phaser.Input.Pointer, sprite: Unit) => {
+                let distance = Phaser.Math.Distance.Between(this.draggedStartX, this.draggedStartY, sprite.x, sprite.y);
 
-            console.log("distance", distance, distance / GRID_SIZE);
+                console.log("distance", distance, distance / GRID_SIZE);
 
-            if (distance > sprite.speed) {
-                console.log("Zuweit!", distance, ">", sprite.speed);
-                sprite.setPosition(this.draggedStartX, this.draggedStartY);
-            }
+                if (distance > sprite.speed) {
+                    console.log("Zuweit!", distance, ">", sprite.speed);
+                    sprite.setPosition(this.draggedStartX, this.draggedStartY);
+                }
 
-            this.draggedStartX = -1;
-            this.draggedStartY = -1;
-        });
+                this.draggedStartX = -1;
+                this.draggedStartY = -1;
+            });
+        }
+        // DRAG LOGIC END
     }
 }
 
